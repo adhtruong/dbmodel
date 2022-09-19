54974b1faf059511c6aa4f7fe1b958762300b1cf
Andrew Truong <adh.truong@gmail.com>
2022-09-19 11:31:44 +0100

    Add relationship

---
+++ db_model/relationship.py
@@ -0,0 +1,49 @@
+from dataclasses import MISSING, Field
+from typing import Any, Callable, Dict, TypeVar
+
+from sqlalchemy.orm import RelationshipProperty
+from sqlalchemy.orm import relationship as _relationship
+from typing_extensions import ParamSpec
+
+T = TypeVar("T")
+
+
+class RelationshipInfo(Field[T]):
+    def __init__(
+        self,
+        *args,
+        **kwargs,
+    ) -> None:
+        field_kwargs: Dict[str, Any] = dict(
+            default=None,
+            default_factory=MISSING,
+            init=True,
+            repr=False,
+            hash=False,
+            compare=False,
+            metadata=None,
+        )
+        if kwargs.get("uselist", False):
+            field_kwargs.update(
+                default=MISSING,
+                default_factory=list,
+            )
+
+        super().__init__(**field_kwargs)  # type: ignore
+        self.args = args
+        self.kwargs = kwargs
+
+    def __getrelationship__(self) -> RelationshipProperty:
+        return _relationship(*self.args, **self.kwargs)
+
+
+P = ParamSpec("P")
+
+
+def copy_t(_: Callable[P, Any]) -> Callable[[Callable[..., Any]], Callable[P, Any]]:
+    return lambda f: f
+
+
+@copy_t(_relationship)
+def relationship(*args, **kwargs) -> Any:
+    return RelationshipInfo(*args, **kwargs)
